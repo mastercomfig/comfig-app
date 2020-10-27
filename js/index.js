010edc125d579ef00b178dfcdded52e9883a66ed
mastercoms <mastercoms@tuta.io>
2020-10-27 04:09:52 -0400

    update progress bar widths

--- js/index.js
+++ js/index.js
@@ -11,25 +11,25 @@ $(function() {
 // Init customization example highlighting
 hljs.initHighlightingOnLoad();
 
+function updateBars() {
+    document.getElementById('mastercomfig-bar').style.width = '100%';
+    document.getElementById('comanglia-bar').style.width = '91%';
+    document.getElementById('m0re-bar').style.width = '90%';
+    document.getElementById('chris-bar').style.width = '84%';
+    document.getElementById('rhapsody-bar').style.width = '80%';
+}
+
 // If we scroll down to the bars, do an anim
 document.addEventListener('scroll', function() {
     if ($('#comanglia-bar').visible()) {
-        document.getElementById('mastercomfig-bar').style.width = '100%';
-        document.getElementById('comanglia-bar').style.width = '95%';
-        document.getElementById('m0re-bar').style.width = '94%';
-        document.getElementById('chris-bar').style.width = '88%';
-        document.getElementById('rhapsody-bar').style.width = '83%';
+        updateBars();
         $(window).off('scroll');
     }
 }, {passive: true});
 
 // If page loaded with bars visible, then still do the anim
 if ($('#comanglia-bar').visible()) {
-    document.getElementById('mastercomfig-bar').style.width = '100%';
-    document.getElementById('comanglia-bar').style.width = '94%';
-    document.getElementById('m0re-bar').style.width = '93%';
-    document.getElementById('chris-bar').style.width = '87%';
-    document.getElementById('rhapsody-bar').style.width = '82%';
+    updateBars();
     $(window).off('scroll');
 }
 
