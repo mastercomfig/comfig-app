0d3c9569a1a253b045403e91bb2ff3b5ec7fdb9e
mastercoms <mastercoms@tuta.io>
2022-05-21 23:04:48 -0400

    some clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), partytown()]
+  integrations: []
 });
