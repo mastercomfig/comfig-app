8fa02b39f52cd87db1247ce9df24d73697715942
mastercoms <mastercoms@tuta.io>
2022-05-26 23:08:36 -0400

    finish purge cloudflare script, remove unused integrations

--- scripts/purge-cloudflare.js
+++ scripts/purge-cloudflare.js
@@ -1,10 +1,50 @@
-var glob = require("glob");
+const glob = require("glob");
+const https = require('https');
+
+const MAX_CF_URLS = 30;
+
+const API_KEY = process.env.CF_API_KEY;
+const API_EMAIL = process.env.CF_API_EMAIL;
+const headers = {
+  'Authorization': `Bearer ${API_KEY}`,
+  'X-Auth-Email': API_EMAIL,
+  'Content-Type': 'application/json',
+}
+
+function purgeOnCloudflare(files) {
+  const data = JSON.stringify({
+    files: files.map(f => `https://mastercomfig.com/${f}`)
+  });
+  return;
+  const options = {
+      hostname: "api.cloudflare.com",
+      port: 443,
+      path: `/client/v4/zones/${process.env.CF_ZONE_ID}/purge_cache`,
+      method: "POST",
+      headers,
+    };
+    const req = https.request(options, (res) => {
+      res.on('data', (d) => {
+        process.stdout.write(d);
+      });
+    });
+    req.write(data);
+    req.end();
+}
 
 glob("src/pages/*.astro", (err, files) => {
-  const options = {
-    hostname: "api.cloudflare.com"
-    port: 443,
-    path: "",
-    method: "POST"
-  };
-});  
+  if (err) {
+    console.warn(err);
+    return;
+  }
+  const html_files = files.map(f => `${f.substring(f.lastIndexOf('/') + 1, f.lastIndexOf('.'))}.html`)
+  files = files.map(f => f.endsWith("index.astro") ? '' : `${f.substring(f.lastIndexOf('/') + 1, f.lastIndexOf('.'))}`);
+  files = files.concat(html_files);
+  if (files.length > MAX_CF_URLS) {
+    while (files.length) {
+      purgeOnCloudflare(files.splice(0, MAX_CF_URLS));
+    }
+  } else {
+    purgeOnCloudflare(files);
+  }
+});
