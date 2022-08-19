3fb45d0d9e708b34bc739a7df80ecc9a162982e7
mastercoms <mastercoms@tuta.io>
2022-08-18 21:52:44 -0400

    get storage working, order items correctly, selection improved

---
+++ src/utils/useHydration.js
@@ -0,0 +1,19 @@
+const useHydration = (useBoundStore) => {
+  const [hydrated, setHydrated] = useState(useBoundStore.persist.hasHydrated)
+
+  useEffect(() => {
+    const unsubHydrate = useBoundStore.persist.onHydrate(() => setHydrated(false)) // Note: this is just in case you want to take into account manual rehydrations. You can remove this if you don't need it/don't want it.
+    const unsubFinishHydration = useBoundStore.persist.onFinishHydration(() => setHydrated(true))
+
+    setHydrated(useBoundStore.persist.hasHydrated())
+
+    return () => {
+      unsubHydrate()
+      unsubFinishHydration()
+    }
+  }, [])
+
+  return hydrated;
+}
+
+export default useHydration;
