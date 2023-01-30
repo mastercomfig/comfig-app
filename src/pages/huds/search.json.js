7825b0009dab0a3f0ebfa959230328bde6d18d28
mastercoms <mastercoms@tuta.io>
2023-01-30 03:28:18 -0500

    add search

--- src/pages/huds/search.json.js
+++ src/pages/huds/search.json.js
@@ -3,12 +3,28 @@ import removeMd from 'remove-markdown';
 
 export const get = async function get() {
   const allHuds = await fetchHuds(true);
-  const hudData = allHuds.map((hud) => ({
-    title: hud.name,
-    author: hud.author,
-    content: removeMd(hud.content),
-    flags: hud.flags,
-    tags: hud.tags,
-  }));
+  const hudData = allHuds.map((hud) => {
+    const data = {
+      id: hud.id,
+      name: hud.name,
+      author: hud.author,
+      content: removeMd(hud.content).toLowerCase(),
+      flags: hud.flags || [],
+      tags: hud.tags || [],
+      bannerUrl: hud.bannerUrl,
+    };
+    const traits = ["os", "gamemode", "res"];
+    if (hud.traits) {
+      for (const [trait, traitVal] of Object.entries(hud.traits)) {
+        data[trait] = traitVal;
+      }
+    }
+    for (const trait of traits) {
+      if (!data[trait]) {
+        data[trait] = [];
+      }
+    }
+    return data;
+  });
   return { body: JSON.stringify(hudData) };
 };
