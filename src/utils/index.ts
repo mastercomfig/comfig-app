121676e91a735565898a15c05160eb09f69a21ab
mastercoms <mastercoms@tuta.io>
2024-07-19 11:58:10 -0400

    fix homepage bugs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,28 +1,30 @@
 import hljs from "highlight.js/lib/core";
 import cpp from "highlight.js/lib/languages/cpp";
 
-function updateBarWidth(id, width) {
-  const el = document.getElementById(id);
-  el.style.width = `${width}%`;
-  el.style.willChange = "auto";
-}
-
-function updateBars(entries) {
-  if (!entries[0].isIntersecting) {
-    return;
+export function homePage() {
+  function updateBarWidth(id, width) {
+    const el = document.getElementById(id);
+    el.style.width = `${width}%`;
+    el.style.willChange = "auto";
   }
-  updateBarWidth("mastercomfig-bar", 100);
-  updateBarWidth("comanglia-bar", 91);
-  updateBarWidth("m0re-bar", 90);
-  updateBarWidth("chris-bar", 84);
-  updateBarWidth("rhapsody-bar", 80);
-  barsObserver.disconnect();
+
+  function updateBars(entries) {
+    if (!entries[0].isIntersecting) {
+      return;
+    }
+    updateBarWidth("mastercomfig-bar", 100);
+    updateBarWidth("comanglia-bar", 91);
+    updateBarWidth("m0re-bar", 90);
+    updateBarWidth("chris-bar", 84);
+    updateBarWidth("rhapsody-bar", 80);
+    barsObserver.disconnect();
+  }
+
+  // If bars are visible, do an anim
+  const barsObserver = new IntersectionObserver(updateBars, { threshold: 1.0 });
+  barsObserver.observe(document.getElementById("mastercomfig-bar"));
+
+  // Init customization example highlighting
+  hljs.registerLanguage("cpp", cpp);
+  hljs.highlightAll();
 }
-
-// If bars are visible, do an anim
-const barsObserver = new IntersectionObserver(updateBars, { threshold: 1.0 });
-barsObserver.observe(document.getElementById("mastercomfig-bar"));
-
-// Init customization example highlighting
-hljs.registerLanguage("cpp", cpp);
-hljs.highlightAll();
