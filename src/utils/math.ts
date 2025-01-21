cf11651a87d4dfcde4f6ff7f4e69170344052458
mastercoms <mastercoms@tuta.io>
2025-01-21 16:15:12 -0500

    fix some map thumbnail issues

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,12 +1,12 @@
-export function expOut(t) {
+export function expOut(t: number) {
   if (t >= 1.0) return 1.0;
   return 1.0 - Math.pow(2.0, -10.0 * t);
 }
 
-export function easeIn(t) {
+export function easeIn(t: number) {
   return Math.pow(t, 1.1);
 }
 
-export function getGrad(t) {
+export function getGrad(t: number) {
   return 1.0 - expOut(t);
 }
