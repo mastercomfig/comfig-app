cb0efeeff8e5dcbbabdc7b446da317d1e9e87cec
mastercoms <mastercoms@tuta.io>
2025-04-02 00:26:55 -0400

    various fixes

---
+++ src/utils/giftCard.ts
@@ -0,0 +1,307 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  AuthErrorCodes,
+  type NextOrObserver,
+  type User,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import {
+  type Unsubscribe,
+  getDatabase,
+  increment,
+  onValue,
+  push,
+  ref,
+  runTransaction,
+  set,
+  update,
+} from "firebase/database";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATiSBWksyFiqduyEjrbAUGbMIp4Qq7N-s",
+  authDomain: "comfig-giftcards.firebaseapp.com",
+  projectId: "comfig-giftcards",
+  storageBucket: "comfig-giftcards.firebasestorage.app",
+  messagingSenderId: "456106717482",
+  appId: "1:456106717482:web:67d624e2e4623cddeeee53",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export function giftSignIn(
+  username: string,
+  password: string,
+  callback: (user: User | undefined, err: undefined) => void,
+) {
+  const email = `${username}@gifts.comfig.app`;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      callback(user, undefined);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      // We don't need to sign up, try signing in
+      if (errorCode === AuthErrorCodes.EMAIL_EXISTS) {
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            callback(user, undefined);
+          })
+          .catch((error) => {
+            callback(undefined, error);
+          });
+      } else {
+        callback(undefined, error);
+      }
+    });
+}
+
+export function subscribeAuth(callback: NextOrObserver<User>) {
+  return onAuthStateChanged(auth, callback);
+}
+
+export function redirectOnAuth() {
+  const unsubscribe: { fn: (() => void) | undefined } = { fn: undefined };
+  function subscription(user) {
+    if (user) {
+      if (unsubscribe.fn) {
+        unsubscribe.fn();
+      }
+      window.location.href = "/giftcard/store";
+    }
+  }
+  unsubscribe.fn = subscribeAuth(subscription);
+}
+
+// Initialize Realtime Database and get a reference to the service
+const db = getDatabase(app);
+
+const subscriptions: Record<string, Unsubscribe> = {};
+
+function handleSub(id: string, newSub: Unsubscribe) {
+  if (subscriptions[id]) {
+    subscriptions[id]();
+    delete subscriptions[id];
+  }
+  subscriptions[id] = newSub;
+}
+
+export const items = ["ticket", "key", "tux", "pepper", "hat", "pan"];
+
+export const expirationTime = {
+  ticket: 300,
+  key: 600,
+  tux: 900,
+  pepper: 1200,
+  hat: 1500,
+  pan: 1800,
+};
+
+interface InventoryAdd {
+  ticket?: number;
+  key?: number;
+  tux?: number;
+  pepper?: number;
+  hat?: number;
+  pan?: number;
+}
+
+export function subscribeWallet(
+  userId: string,
+  callback: (data: object | null) => void,
+) {
+  const walletRef = ref(db, `wallet/${userId}`);
+  handleSub(
+    "wallet",
+    onValue(walletRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    }),
+  );
+}
+
+export function subscribeAllWallets(callback: (data: object | null) => void) {
+  const walletRef = ref(db, "wallet");
+  handleSub(
+    "wallets",
+    onValue(walletRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    }),
+  );
+}
+
+export function updateWallet(userId: string, funds: number) {
+  const updates = {
+    [`wallet/${userId}/funds`]: increment(funds),
+  };
+  return update(ref(db), updates);
+}
+
+export function subscribeInventory(
+  userId: string,
+  callback: (data: object | null) => void,
+) {
+  const inventoryRef = ref(db, `inventory/${userId}`);
+  handleSub(
+    "inventory",
+    onValue(inventoryRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    }),
+  );
+}
+
+export function addInventory(userId: string, inventory: InventoryAdd) {
+  const now = Date.now();
+  for (const [k, v] of Object.entries(inventory)) {
+    runTransaction(ref(db, `inventory/${userId}/${k}`), (item) => {
+      if (item) {
+        item.count += v;
+        if (item.count <= 0) {
+          item.count = 0;
+          item.expire = null;
+        } else if (!item.expire) {
+          item.expire = now + expirationTime[k] * 1000;
+        }
+        return item;
+      } else if (v === 0) {
+        return {
+          count: 0,
+        };
+      } else if (v > 0) {
+        return {
+          count: v,
+          expire: now + expirationTime[k] * 1000,
+        };
+      }
+    });
+  }
+}
+
+export function updateInventoryExpire(userId: string, inventory: InventoryAdd) {
+  const updates = {};
+  const now = Date.now();
+  for (const k of Object.keys(inventory)) {
+    const expire = now + expirationTime[k] * 1000;
+    updates[`inventory/${userId}/${k}/expire`] = expire;
+  }
+  update(ref(db), updates);
+}
+
+export function subscribeListing(callback: (data: object | null) => void) {
+  const listingRef = ref(db, "listing");
+  handleSub(
+    "listing",
+    onValue(listingRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    }),
+  );
+}
+
+export function addListing(userId: string, item: string, price: number) {
+  addInventory(userId, {
+    [item]: -1,
+  });
+  const listingRef = ref(db, `listing/${userId}`);
+  const newListingRef = push(listingRef);
+  set(newListingRef, {
+    item,
+    price,
+  });
+}
+
+export function buyListing(
+  buyerId: string,
+  sellerId: string,
+  listingId: string,
+  item: string,
+  price: number,
+  callback,
+) {
+  // make sure we only adjust wallet once
+  const updates = {
+    [`listing/${sellerId}/${listingId}`]: null,
+    [`wallet/${sellerId}/funds`]: increment(price),
+    [`wallet/${buyerId}/funds`]: increment(-price),
+  };
+  update(ref(db), updates).then(callback);
+  addInventory(buyerId, {
+    [item]: 1,
+  });
+}
+
+export function subscribeRaffle(callback: (data: object | null) => void) {
+  const raffleRef = ref(db, "raffle");
+  handleSub(
+    "raffle",
+    onValue(raffleRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    }),
+  );
+}
+
+export function subscribeRaffleListing(
+  callback: (data: object | null) => void,
+) {
+  const raffleRef = ref(db, "raffleListing");
+  handleSub(
+    "raffleListing",
+    onValue(raffleRef, (snapshot) => {
+      const data = snapshot.val();
+      callback(data);
+    }),
+  );
+}
+
+export function enterRaffle(userId: string, raffleId: string) {
+  const walletRef = ref(db, `wallet/${userId}`);
+  const rafflePrice = -1;
+  let canAfford = true;
+  return runTransaction(walletRef, (item) => {
+    if (item && item.funds !== undefined) {
+      if (item.funds + 50 < 1) {
+        canAfford = false;
+        return {
+          funds: item.funds,
+        };
+      }
+      return {
+        funds: item.funds + rafflePrice,
+      };
+    } else {
+      return {
+        funds: rafflePrice,
+      };
+    }
+  }).then(() => {
+    if (canAfford) {
+      const raffleListingRef = ref(db, `raffleListing/${userId}/${raffleId}`);
+      runTransaction(raffleListingRef, (item) => {
+        console.log(item);
+        if (item) {
+          item++;
+          return item;
+        } else {
+          return 1;
+        }
+      });
+    }
+  });
+}
---
+++ src/utils/giftCardPage.ts
@@ -0,0 +1,341 @@
+import fastClone from "./fastClone";
+import {
+  addInventory,
+  addListing,
+  buyListing,
+  enterRaffle,
+  items,
+  subscribeAllWallets,
+  subscribeAuth,
+  subscribeInventory,
+  subscribeListing,
+  subscribeRaffle,
+  subscribeRaffleListing,
+  subscribeWallet,
+  updateInventoryExpire,
+} from "./giftCard";
+
+export function giftCard() {
+  const walletEl = document.getElementById("giftCardWallet") as HTMLElement;
+  const inventoryEls: Record<string, HTMLElement> = {};
+  const marketEls: Record<string, HTMLElement> = {};
+  for (const item of items) {
+    inventoryEls[item] = document.getElementById(
+      `my-item-${item}`,
+    ) as HTMLElement;
+    marketEls[item] = document.getElementById(`item-${item}`) as HTMLElement;
+  }
+  const raffleEl = document.getElementById("giftCardRaffle") as HTMLElement;
+  const leaderboardEl = document.getElementById(
+    "giftCardLeaderboard",
+  ) as HTMLElement;
+  const timeouts = {};
+
+  let hasAuthed = false;
+
+  function addTimeout(userId, item, timeout) {
+    if (timeouts[item] !== undefined) {
+      return;
+    }
+    const now = Date.now();
+    const timer = timeout - now;
+    function onTimeout() {
+      // clear out
+      delete timeouts[item];
+      // remove an item
+      addInventory(userId, { [item]: -1 });
+      // update expiration time
+      updateInventoryExpire(userId, { [item]: 0 });
+    }
+    if (timer <= 0) {
+      onTimeout();
+      return;
+    }
+    const handle = setTimeout(onTimeout, timer);
+    timeouts[item] = handle;
+  }
+
+  const defaultInventory = {
+    ticket: { count: 0 },
+    key: { count: 0 },
+    tux: { count: 0 },
+    pepper: { count: 0 },
+    hat: { count: 0 },
+    pan: { count: 0 },
+  };
+
+  function ordinalSuffixOf(i) {
+    const j = i % 10;
+    const k = i % 100;
+    if (j === 1 && k !== 11) {
+      return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+      return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+      return i + "rd";
+    }
+    return i + "th";
+  }
+
+  function onUser(user) {
+    let walletTotal = 0;
+    subscribeWallet(user.uid, (data) => {
+      const promo = 50;
+      const funds = data?.["funds"] ?? 0;
+      walletTotal = promo + funds;
+      walletEl.innerText = `$${walletTotal.toFixed(2)}`;
+    });
+    subscribeAllWallets((data) => {
+      if (!data) {
+        data = {
+          [user.uid]: {
+            funds: 0,
+          },
+        };
+      }
+      const wallets = [];
+      for (const [uid, wallet] of Object.entries(data)) {
+        const funds = wallet?.["funds"] ?? 0;
+        wallets.push({ uid, funds: Math.max(funds, 0) });
+      }
+      console.log(wallets);
+      wallets.sort(
+        (a, b) => b.funds - a.funds || a.toString().localeCompare(b.toString()),
+      );
+      const place =
+        wallets.findIndex((element) => element.uid === user.uid) + 1;
+      leaderboardEl.innerHTML = `<p>You are in ${ordinalSuffixOf(place)} place.`;
+    });
+    const selling = { v: false };
+    subscribeInventory(user.uid, (data) => {
+      if (!data) {
+        data = {};
+      }
+      data = { ...defaultInventory, ...data };
+      selling.v = false;
+      for (const [k, v] of Object.entries(data)) {
+        const myItem = inventoryEls[k];
+        const sellPrice = document.getElementById(
+          `sell-price-${k}`,
+        ) as HTMLInputElement;
+        const sellBtn = document.getElementById(
+          `sell-btn-${k}`,
+        ) as HTMLButtonElement;
+        const curCount = myItem?.querySelector(".cur-count") as HTMLElement;
+        if (v.count > 0) {
+          addTimeout(user.uid, k, v.expire);
+          myItem?.classList.add("has-items");
+          myItem?.classList.remove("no-items");
+          sellPrice.disabled = false;
+          sellBtn.disabled = false;
+          sellBtn.onclick = () => {
+            if (selling.v) {
+              return;
+            }
+            const sale = Number(Number(sellPrice.value).toFixed(2));
+            if (sale < 0.03) {
+              return;
+            }
+            addListing(user.uid, k, sale);
+          };
+          curCount.innerText = String(v.count);
+        } else {
+          myItem?.classList.add("no-items");
+          myItem?.classList.remove("has-items");
+          sellPrice.disabled = true;
+          sellBtn.disabled = true;
+          sellBtn.onclick = () => {};
+          curCount.innerText = "0";
+        }
+      }
+    });
+    let bestListing = {};
+    const buying = { v: false };
+    subscribeListing((data) => {
+      if (!data) {
+        data = {};
+      }
+      const diff = fastClone(bestListing);
+      for (const [k, v] of Object.entries(diff)) {
+        diff[k] = v.price;
+      }
+      bestListing = {};
+      for (const [sellerId, listings] of Object.entries(data)) {
+        if (sellerId === user.uid) {
+          continue;
+        }
+        for (const [listingId, listing] of Object.entries(listings)) {
+          const bestListingItem = bestListing[listing.item];
+          const bestListingPrice = bestListingItem?.price ?? Infinity;
+          if (listing.price < bestListingPrice) {
+            bestListing[listing.item] = {
+              sellerId,
+              listingId,
+              price: listing.price,
+            };
+          }
+        }
+      }
+      for (const itemKey of Object.keys(diff)) {
+        if (bestListing[itemKey] === undefined) {
+          delete diff[itemKey];
+        }
+      }
+      for (const itemKey of Object.keys(bestListing)) {
+        diff[itemKey] = bestListing[itemKey].price - (diff[itemKey] ?? 0);
+      }
+      for (const [item, itemEl] of Object.entries(marketEls)) {
+        const listing = bestListing[item];
+        const curPrice = itemEl.querySelector(".cur-price") as HTMLElement;
+        const diffPrice = itemEl.querySelector(".diff-price") as HTMLElement;
+        if (listing === undefined) {
+          itemEl.onclick = () => {};
+          itemEl.classList.add("no-items");
+          itemEl.classList.remove("has-items");
+          curPrice.innerText = "";
+          diffPrice.innerText = "";
+          continue;
+        }
+        itemEl.classList.remove("no-items");
+        itemEl.classList.add("has-items");
+        const delta = diff[item] ?? 0;
+        curPrice.innerText = `$${listing.price.toFixed(2)}`;
+        if (delta === 0) {
+          diffPrice.innerText = "";
+        } else {
+          if (delta > 0) {
+            diffPrice.innerText = `+${Number(delta).toFixed(2)}`;
+            diffPrice.classList.add("text-success");
+            diffPrice.classList.remove("text-danger");
+          } else {
+            diffPrice.innerText = `-${Number(delta).toFixed(2)}`;
+            diffPrice.classList.add("text-danger");
+            diffPrice.classList.remove("text-success");
+          }
+        }
+        itemEl.onclick = () => {
+          if (buying.v) {
+            return;
+          }
+          buying.v = true;
+          if (walletTotal >= listing.price) {
+            buyListing(
+              user.uid,
+              listing.sellerId,
+              listing.listingId,
+              item,
+              listing.price,
+              () => {
+                buying.v = false;
+              },
+            );
+          }
+        };
+      }
+    });
+    subscribeRaffle((data) => {
+      if (!data) {
+        raffleEl.innerHTML = "<p>No raffles available</p>";
+        return;
+      }
+      raffleEl.innerHTML = "";
+      for (const [raffleId, raffle] of Object.entries(data)) {
+        const container = document.createElement("div");
+        const row = document.createElement("div");
+        row.classList.add("row");
+        const left = document.createElement("div");
+        left.classList.add("col");
+        const right = document.createElement("div");
+        right.classList.add("col");
+        const mid = document.createElement("div");
+        mid.classList.add("col");
+        const button = document.createElement("button");
+        const para = document.createElement("p");
+        const counter = document.createElement("p");
+        counter.innerText = "0 / 0";
+        counter.id = `count_${raffleId}`;
+        mid.appendChild(counter);
+        left.appendChild(para);
+        right.appendChild(button);
+        row.appendChild(left);
+        row.appendChild(mid);
+        row.appendChild(right);
+        container.appendChild(row);
+        button.innerText = "Enter raffle ($1)";
+        button.classList.add("btn", "btn-success", "btn-sm");
+        button.onclick = () => {
+          button.disabled = true;
+          enterRaffle(user.uid, raffleId).then(() => {
+            button.disabled = false;
+          });
+        };
+        const now = Date.now();
+        function updateTimer() {
+          const remaining = raffle.raffleTime - now;
+          const minutes = Math.floor(
+            (remaining % (1000 * 60 * 60)) / (1000 * 60),
+          );
+          const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+          para.innerText = `${minutes.toString().padStart(2, "0")}m ${seconds.toString().padStart(2, "0")}s`;
+        }
+        updateTimer();
+        const handle = setInterval(() => {
+          const now = Date.now();
+          const remaining = raffle.raffleTime - now;
+          if (remaining < 0) {
+            if (handle) {
+              clearInterval(handle);
+            }
+            button.remove();
+            return;
+          }
+          const minutes = Math.floor(
+            (remaining % (1000 * 60 * 60)) / (1000 * 60),
+          );
+          const seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+          para.innerText = `${minutes.toString().padStart(2, "0")}m ${seconds.toString().padStart(2, "0")}s`;
+        }, 1000);
+        raffleEl.appendChild(container);
+      }
+    });
+    subscribeRaffleListing((data) => {
+      if (!data) {
+        return;
+      }
+      const raffleCounts = {};
+      for (const [userId, raffles] of Object.entries(data)) {
+        for (const [raffleId, entries] of Object.entries(raffles)) {
+          if (raffleCounts[raffleId] === undefined) {
+            raffleCounts[raffleId] = {
+              mine: 0,
+              theirs: 0,
+            };
+          }
+          if (userId === user.uid) {
+            raffleCounts[raffleId].mine += entries;
+          } else {
+            raffleCounts[raffleId].theirs += entries;
+          }
+        }
+      }
+
+      for (const [raffleId, counts] of Object.entries(raffleCounts)) {
+        const counter = document.getElementById(`count_${raffleId}`);
+        counter.innerText = `${counts.mine} / ${counts.theirs + counts.mine}`;
+      }
+    });
+  }
+
+  subscribeAuth((user) => {
+    if (user) {
+      if (!hasAuthed) {
+        hasAuthed = true;
+        onUser(user);
+      }
+    } else {
+      window.location.href = "/giftcard/";
+    }
+  });
+}
